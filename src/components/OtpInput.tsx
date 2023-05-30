6c75b2492680c23d41e86cbe8f3161bc0bb5e4d7
akarsh_kr <akarsh.kr@cognitiveclouds.com>
2023-05-30 12:06:54 +0530

    validation added

--- src/components/OtpInput.tsx
+++ src/components/OtpInput.tsx
@@ -1,21 +1,24 @@
 import React, { useState, useRef } from "react";
-import { v4 as uuidv4 } from 'uuid';
 import "./OtpInput.css";
 const OtpInputField: React.FC = () => {
   const [otp, setOtp] = useState<string[]>(["", "", "", "", "", ""]);
+  const [error, setError] = useState<string>("");
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    if (value.length <= 6) {
-      const updatedOtp = [...otp];
-      updatedOtp[Number(e.target.name)] = value;
-      setOtp(updatedOtp);
-      console.log("otp", otp);
-      if (value.length === 1 && Number(e.target.name) < 5) {
-        inputRefs.current[Number(e.target.name) + 1]?.focus();
-        console.log(inputRefs.current[Number(e.target.name) + 1])
+    if (/^\d*$/.test(value)) {
+      if (value.length <= 6) {
+        const updatedOtp = [...otp];
+        updatedOtp[Number(e.target.name)] = value;
+        setOtp(updatedOtp);
+        setError("");
+        if (value.length === 1 && Number(e.target.name) < 5) {
+          inputRefs.current[Number(e.target.name) + 1]?.focus();
+        }
       }
+    } else {
+      setError("Please enter only numeric values.");
     }
   };
 
@@ -26,25 +29,45 @@ const OtpInputField: React.FC = () => {
         inputRefs.current[currentIndex - 1]?.focus();
       }
     }
-  }
+  };
+  const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    const pastedData = e.clipboardData.getData("Text");
+    const otpArray = pastedData.split("").slice(0, 6);
+    const isValidOtp = otpArray.every((value) => /^\d*$/.test(value));
+
+    if (isValidOtp) {
+      const updatedOtp = [...otp];
+      otpArray.forEach((value, index) => {
+        updatedOtp[index] = value;
+      });
+      setOtp(updatedOtp);
+      setError("");
+    } else {
+      setError("Please enter only numeric values.");
+    }
+  };
   return (
-    <div className="input_container">
-      {otp.map((value, index) => (
-        <div key={index}>
-          <input
-            type="text"
-            className="otp_input"
-            maxLength={1}
-            name={index.toString()}
-            value={value}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            ref={(ref) => (inputRefs.current[index] = ref)}
-            style={{ width: "1em" }}
-          />
-          {index < otp.length - 1 && <span className="hyphen">-</span>}
-        </div>
-      ))}
+    <div>
+      {error && <div className="error_message">{error}</div>}
+      <div className="input_container">
+        {otp.map((value, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              className="otp_input"
+              maxLength={1}
+              name={index.toString()}
+              value={value}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+              onPaste={handlePaste}
+              ref={(ref) => (inputRefs.current[index] = ref)}
+              style={{ width: "1em" }}
+            />
+            {index < otp.length - 1 && <span className="hyphen">-</span>}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
